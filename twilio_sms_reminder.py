a69d7d5ed2dacfe7d41d83f19f57457f6c202be3
shaunak7567 <shaunak7567@gmail.com>
2017-12-30 11:50:36 -0800

    Delete twilio_sms_reminder.py

--- twilio_sms_reminder.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from twilio.rest import Client
-accountSID='AC69ad35195752b759537ed23caa430e0d'
-authToken='e1bf9e1fb6bbfaa69b1901a570231328'
-twiliocli=Client(accountSID,authToken)
-mytwilionumber='+18316091347'
-mycellphone='+14084807851'
-message=twiliocli.messages.create(body='HI! This is a test message from shaunak!', from_=mytwilionumber,to=mycellphone)
-print(message.sid)
